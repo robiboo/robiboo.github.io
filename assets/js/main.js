5211755a954d790b23a120d5ccc64dfd62ad77f7
robert.panerio <robert.panerio@wsu.edu>
2024-02-20 20:24:45 -0800

    Finished the hamburger menu. Added personal information

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function clickMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".menu-icon");
+
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
